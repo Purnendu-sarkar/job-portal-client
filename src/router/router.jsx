2508b81c3727735c2a4d378b3fc90a959bcf1045
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-12-13 15:17:34 +0600

    Navbar Register Route and Firebase Config added.

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    errorElement: <h2>Not found</h2>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
